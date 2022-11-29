c341af852182edbccea8912d88d10f45d7cc3b7e
Onur Arıkan <onur93arikan@gmail.com>
2022-11-29 17:44:56 +0300

    Updated __setup__.py
    Changed what modules to be included and excluded. Therefore included modules will be more guessable in different environments.

--- __setup__.py
+++ __setup__.py
@@ -1,8 +1,24 @@
 import sys
+import pkgutil
 from cx_Freeze import setup, Executable
 
 sys.argv.append("build")
 # sys.argv.append("bdist_msi")
+
+INCLUDES = [
+    "collections", "encodings", "importlib", "urllib", 
+    "pygame", "pygame.locals", "importlib.resources", "scenes"
+]
+EXCLUDES = list({module.name for module in list(pkgutil.iter_modules()) if module.ispkg} - set(INCLUDES))
+EXCLUDES.extend(
+    [
+        "_queue", "_bz2", "_hashlib", "_lzma", "_socket", "select",
+        'asyncio', 'concurrent', 'ctypes', 'distutils', 'email', 'html',
+        'http', 'lib2to3', 'logging', 'multiprocessing', 'pkg_resources',
+        'pydoc_data', 'test', 'unittest', 'xml', 'xmlrpc',"PyQt4",
+        "PyQt5", 'matplotlib', "numpy", "tkinter", "Tkinter"
+    ]
+)
 
 _executables = [
     Executable(
@@ -16,18 +32,13 @@ _executables = [
 ]
 
 _build_exe_options = dict(
-    build_exe = "Flagsweeper v0.4.2",
+    build_exe = "Flagsweeper v0.4.2.1",
     packages = [],
     include_files =  ["resources"],
     zip_include_packages = ["*"],
     zip_exclude_packages = [],
-    excludes = [
-        "_queue", "_bz2", "_hashlib", "_lzma", "_socket", "select",
-        'asyncio', 'concurrent', 'ctypes', 'distutils', 'email', 'html',
-        'http', 'lib2to3', 'logging', 'multiprocessing', 'pkg_resources',
-        'pydoc_data', 'test', 'unittest', 'xml', 'xmlrpc',"PyQt4",
-        "PyQt5", 'matplotlib', "numpy", "tkinter", "Tkinter"
-    ],
+    includes = INCLUDES,
+    excludes = EXCLUDES,
     replace_paths = [("*", "")],
     optimize = 2,
     include_msvcr = True,
@@ -36,7 +47,7 @@ _build_exe_options = dict(
 
 setup(
     name = "Flagsweeper",
-    version = "0.4.2",
+    version = "0.4.2.1",
     description = "Flagsweeper",
     options = {"build_exe": _build_exe_options},
     executables = _executables
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 """Constants which are imported are here"""
 
-VERSION = "0.4.2"
+VERSION = "0.4.2.1"
 TITLE = f"Flagsweeper v{VERSION}"
 WINDOW_SIZE = (550, 650)
 FPS = 30
